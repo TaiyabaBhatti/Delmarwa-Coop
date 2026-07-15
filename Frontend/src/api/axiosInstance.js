041b092c71e20df935764c6b63d11b9a6b1d18ba
TaiyabaBhatti <taiyababhattii@gmail.com>
2026-07-15 16:46:38 +0500

    testing

--- Frontend/src/api/axiosInstance.js
+++ Frontend/src/api/axiosInstance.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  // baseURL: import.meta.env.VITE_API_URL,
-  // withCredentials: true,
+  withCredentials: true,
 });
 
 export default axiosInstance;
